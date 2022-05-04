595fffbe82b01201eea98929b220e6d3bef471ea
dobschal <s.dobschal@tdsoftware.de>
2022-05-04 21:53:23 +0200

    show info about user based on search

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { getIdentities, searchIdentities, implementsIdentityPallet  } from "./identities";
+import { getIdentities, searchIdentities, implementsIdentityPallet } from "./identities";
 import { BasicIdentityInfo } from "./types/BasicIdentityInfo";
 import { Identity } from "./types/Identity";
 import { Page } from "./types/Page";
 
 export {
     getIdentities, implementsIdentityPallet, BasicIdentityInfo, Identity, Page, searchIdentities
-    getIdentities, implementsIdentityPallet, BasicIdentityInfo, Identity, Page
 };
