84491bd86947464de0cde296295d75d790b15488
Vani Narayana <v.narayana@tdsoftware.de>
2022-05-10 09:04:57 +0200

    updated description of isArchiveNode()

--- src/utilities.ts
+++ src/utilities.ts
@@ -13,13 +13,15 @@ export async function _connectToWsProvider(wsAddress: string): Promise<ApiPromis
 }
 
 /**
- * By default a Substrate node will only keep state for the last 256 blocks, unless it is explicitly run in archive mode. 
- * This means that querying state further back than the pruning period will result in an error returned from the Node.
- * This method uses this information to identify if a node is running in archive or full mode.
+ * check if the node of provided wsProvider is running on archive mode 
+ * @param wsAddress Network end point URL
+ * @returns true if node is running in archive mode
  */
 export const isArchiveNode = async (wsAddress : string): Promise<boolean> => {
     const api = await _connectToWsProvider(wsAddress);
     const historyBlockHash = await api.rpc.chain.getBlockHash(1); //Hash of Block#1
+    
+    //Archive nodes have all the historical data of the blockchain since the genesis block
     try {
         //Query state of a history block
         await api.at(historyBlockHash);
